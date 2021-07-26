0c133c9088b75b26ee10cbc7ca290d6c7527365a
mickey01code <mickey01mail@pm.me>
2021-07-26 12:02:38 +0000

    перенос данных для фильмов

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,7 +49,7 @@ document.getElementById("mentions").innerHTML = "&copy; " + anyear + " - " + yea
 const baseurl = "https://streamsudo.netlify.app/";
 
 const Info = async () => {
-    url = "assets/index.json";
+    url = "https://raw.githubusercontent.com/streamsudo/streamsudo.github.io/main/_data/index.json";
     if (typeof url === "undefined") {
         console.log("Aucune url définie...");
     } else {
